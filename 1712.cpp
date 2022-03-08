204c10ba923a6b0f3ad0f28259c58d2812a134e8
Jung Kwon <89067942+sadad64@users.noreply.github.com>
2022-03-08 23:56:08 +0900

    Create 1712.cpp

---
+++ 1712.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int a;
+	int b;
+	int c;
+	int i;
+
+	cin >> a >> b >> c;
+
+	if (b >= c) {
+		cout << "- 1";
+	}
+	else {
+		i = a / (c - b) + 1;
+	}
+
+	cout << i;
+
+	return 0;
+}
